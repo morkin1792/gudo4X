2e65c3922ab08d6ead331a56f0ddbe30120615de
Vinicius <17213356+morkin1792@users.noreply.github.com>
2021-06-04 18:00:31 -0300

    added support for serving other file types

--- server.py
+++ server.py
@@ -1,37 +1,48 @@
 from flask import Flask, request, make_response
 from datetime import datetime
-import sys, os, re
+import sys, os, re, magic
 
-port = 443
-log_dir='./logs'
-scripts_location = './scripts'
+log_directory='./logs'
+static_directory = './static'
 
+app = Flask(__name__, static_url_path='')
 
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+@app.after_request
+def remove_header(response):
+    response.headers['Server'] = 'generic'
+    return response
 
-class myFlask(Flask):
-    def process_response(self, response):
-        response.headers['server'] = 'Generic'
-        return(response)
+@app.route('/')
+def home():
+    return ':p'
 
-app = myFlask(__name__, static_url_path='')
+@app.errorhandler(Exception)
+def handle(e):
+    return ':o'
+
+def get_content_type(filepath):
+    mime = magic.Magic(mime=True)
+    if '.js' in filepath:
+        return 'application/javascript'
+    else:
+        return mime.from_file(filepath)
 
 @app.route('/<name>', methods=['GET'])
-def script(name):
+def load_static(name):
     try:
         host = request.headers.get('host')
         if len(host) < 1:
             host = request.headers.get('Host')
-        name = re.search('[a-zA-Z0-9.]+', name)
+        name = re.search('[a-zA-Z0-9_.-]+', name)
         name = name.group()
-        script = open(scripts_location + '/' + name).read()
+        filepath = static_directory + '/' + name
+        content = open(filepath, 'rb').read()
         if len(host) > 0:
-            script = script.replace('myserverhere', host)
-        resp = make_response(script)
-        resp.headers['Content-Type'] = 'application/javascript; charset=utf-8'
-        resp.headers['Cache-Control'] = 'no-cache'
-        return resp
+            content = content.replace(b'myserverhere', bytes(host, 'utf8'))
+        response = make_response(content)
+        response.headers['Content-Type'] = get_content_type(filepath)
+        response.headers['Cache-Control'] = 'no-cache'
+        return response
     except Exception: 
         return ':p'
 
@@ -46,7 +57,7 @@ def infos():
         if client_ip == '127.0.0.1':
             client_ip = request.headers.get('X-Forwarded-For')
         client_ip = re.match(ip_rgx, client_ip).group()
-        log = open(log_dir + '/' + client_ip + '__' + datetime.now().strftime('%d.%m.%Y__%H.%M.%S__%f') + '.log', 'wb') 
+        log = open(log_directory + '/' + client_ip + '__' + datetime.now().strftime('%d.%m.%Y__%H.%M.%S__%f') + '.log', 'wb') 
         log.write(bytes('* [' + datetime.now().strftime('%d/%m/%Y %H:%M:%S') + ' @ ' + client_ip + '] ' + metadata + '\n', 'utf-8'))
         log.write(request.get_data())
         log.close()
@@ -60,11 +71,22 @@ def infos():
     except Exception:
         return ':p'
 
-@app.route('/')
-def home():
-    return ':p'
+def get_port(port):
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    return str(port)
 
-if len(sys.argv) > 1:
-    port = sys.argv[1]
-app.run(port=port, host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
-
+def main():
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+    try:
+        if os.path.isfile('cert.pem') and os.path.isfile('key.pem'):
+            app.run(port=get_port(443), host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
+        else:
+            print(' * ssl cert files not found, it will run in plaintext')
+            app.run(port=get_port(80), host='0.0.0.0')
+    except PermissionError:
+        print('\n[!] permission error, try execute as root or change the port')
+            
+if __name__ == '__main__':
+    main()
